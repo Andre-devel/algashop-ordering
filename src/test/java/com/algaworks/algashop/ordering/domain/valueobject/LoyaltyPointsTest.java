443e5e2c83381d3764a4b780457158eb534c46be
Andre goncalves <andrevilanovagon@hotmail.com>
2025-07-16 20:26:54 -0300

    feat: add test LoyaltyPoints

---
+++ src/test/java/com/algaworks/algashop/ordering/domain/valueobject/LoyaltyPointsTest.java
@@ -0,0 +1,29 @@
+package com.algaworks.algashop.ordering.domain.valueobject;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LoyaltyPointsTest {
+    
+    @Test
+    void shouldGenerate() {
+        LoyaltyPoints loyaltyPoints = new LoyaltyPoints(10);
+        Assertions.assertThat(loyaltyPoints.value()).isEqualTo(10);
+    }
+
+    @Test
+    void shouldAddValue() {
+        LoyaltyPoints loyaltyPoints = new LoyaltyPoints(10);
+        LoyaltyPoints LoyaltyPointsUpdated = loyaltyPoints.add(10);
+        Assertions.assertThat(LoyaltyPointsUpdated.value()).isEqualTo(20);
+    }
+
+    @Test
+    void shouldNotAddValue() {
+        LoyaltyPoints loyaltyPoints = new LoyaltyPoints(10);
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                        .isThrownBy(() -> loyaltyPoints.add(-10));
+        Assertions.assertThat(loyaltyPoints.value()).isEqualTo(10);
+    }
+}
