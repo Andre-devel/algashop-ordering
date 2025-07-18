8fda7d916cdf9d70b9b3e54533aa52daaecdbfa7
Andre goncalves <andrevilanovagon@hotmail.com>
2025-07-17 21:26:16 -0300

    feat: add news valueObjects Address and ZipCode

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/valueobject/Address.java
@@ -0,0 +1,33 @@
+package com.algaworks.algashop.ordering.domain.valueobject;
+
+import com.algaworks.algashop.ordering.domain.validator.FieldValidations;
+import lombok.Builder;
+
+import java.util.Objects;
+
+@Builder(toBuilder = true)
+public record Address(
+        String street,
+        String complement,
+        String neighborhood,
+        String number,
+        String city,
+        String state,
+        ZipCode zipCode
+) {
+    
+    public Address {
+        FieldValidations.requireNotBlank(street);
+        FieldValidations.requireNotBlank(neighborhood);
+        FieldValidations.requireNotBlank(number);
+        FieldValidations.requireNotBlank(city);
+        FieldValidations.requireNotBlank(state);
+        Objects.requireNonNull(zipCode);
+        
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s, %s, %s, %s - %s", street, complement, neighborhood, number, city, state, zipCode);
+    }
+}
