b06c67f0289bd299ab4d918e9d869f28bc6089a0
Andre goncalves <andrevilanovagon@hotmail.com>
2025-09-25 21:11:53 -0300

    feat: add ModelMapper integration for CustomerOutput conversion and create Mapper interface

--- src/main/java/com/algaworks/algashop/ordering/aplication/customer/management/CustomerManagementApplicationService.java
+++ src/main/java/com/algaworks/algashop/ordering/aplication/customer/management/CustomerManagementApplicationService.java
@@ -1,6 +1,7 @@
 package com.algaworks.algashop.ordering.aplication.customer.management;
 
 import com.algaworks.algashop.ordering.aplication.commons.AddressData;
+import com.algaworks.algashop.ordering.aplication.utility.Mapper;
 import com.algaworks.algashop.ordering.domain.model.commons.Address;
 import com.algaworks.algashop.ordering.domain.model.commons.Document;
 import com.algaworks.algashop.ordering.domain.model.commons.Email;
@@ -26,6 +27,7 @@ public class CustomerManagementApplicationService {
     
     private final CustomerRegistrationService customerRegistrationService;
     private final Customers customers;
+    private final Mapper mapper;
     
     @Transactional
     public UUID create(CustomerInput input) {
@@ -60,27 +62,6 @@ public class CustomerManagementApplicationService {
         Objects.requireNonNull(customerId);
         Customer customer = customers.ofId(new CustomerId(customerId)).orElseThrow(() -> new CustomerNotFoundException("Customer not found"));
         
-        return CustomerOutput.builder()
-                .id(customer.id().value())
-                .firstName(customer.fullName().firstName())
-                .lastName(customer.fullName().lastName())
-                .email(customer.email().value())
-                .document(customer.document().value())
-                .phone(customer.phone().value())
-                .promotionNotificationsAllowed(customer.isPromotionNotificationsAllowed())
-                .loyaltyPoints(customer.loyaltyPoints().value())
-                .registeredAt(customer.registeredAt())
-                .archivedAt(customer.archivedAt() != null ? customer.archivedAt() : null)
-                .birthDate(customer.birthDate().value() != null ? customer.birthDate().value() : null)
-                .address(AddressData.builder()
-                        .street(customer.address().street())
-                        .number(customer.address().number())
-                        .complement(customer.address().complement())
-                        .neighborhood(customer.address().neighborhood())
-                        .city(customer.address().city())
-                        .state(customer.address().state())
-                        .zipCode(customer.address().zipCode().value())
-                        .build())
-                .build();
+        return mapper.convert(customer, CustomerOutput.class);
     }
 }
---
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/utility/modelmapper/ModelMapperConfig.java
@@ -0,0 +1,83 @@
+package com.algaworks.algashop.ordering.infrastructure.utility.modelmapper;
+
+import com.algaworks.algashop.ordering.aplication.customer.management.CustomerOutput;
+import com.algaworks.algashop.ordering.aplication.utility.Mapper;
+import com.algaworks.algashop.ordering.domain.model.commons.FullName;
+import com.algaworks.algashop.ordering.domain.model.customer.BirthDate;
+import com.algaworks.algashop.ordering.domain.model.customer.Customer;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.convention.NamingConventions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+
+@Configuration
+public class ModelMapperConfig {
+    
+    private static final Converter<FullName, String> fullNameToFistNameConverter = mappingContext -> {
+        FullName fullName = mappingContext.getSource();
+        
+        if (fullName == null) {
+            return null;
+        }
+        
+        return fullName.firstName();
+    };
+
+    private static final Converter<FullName, String> fullNameToLastNameConverter = mappingContext -> {
+        FullName fullName = mappingContext.getSource();
+
+        if (fullName == null) {
+            return null;
+        }
+
+        return fullName.lastName();
+    };
+
+    private static final Converter<BirthDate, LocalDate> birthDateToLocalDateConverter = mappingContext -> {
+        BirthDate birthDate = mappingContext.getSource();
+        if (birthDate == null) {
+            return null;
+        }
+
+        return birthDate.value();
+    };
+    
+    @Bean
+    public Mapper mapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        
+        configuration(modelMapper);
+        
+        return new Mapper() {
+            @Override
+            public <T> T convert(Object object, Class<T> destinationClass) {
+                return modelMapper.map(object, destinationClass);
+            }
+        };
+    }
+
+    private void configuration(ModelMapper modelMapper) {
+        modelMapper.getConfiguration()
+                .setSourceNamingConvention(NamingConventions.NONE)
+                .setDestinationNamingConvention(NamingConventions.NONE)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        
+        modelMapper.createTypeMap(Customer.class, CustomerOutput.class)
+                .addMappings(mapping -> 
+                        mapping.using(fullNameToFistNameConverter)
+                                .map(Customer::fullName, CustomerOutput::setFirstName)
+                )
+                .addMappings(mapping ->
+                        mapping.using(fullNameToLastNameConverter)
+                                .map(Customer::fullName, CustomerOutput::setLastName)
+                )
+                .addMappings(mapping ->
+                        mapping.using(birthDateToLocalDateConverter)
+                                .map(Customer::birthDate, CustomerOutput::setBirthDate)
+                );
+    }
+}
