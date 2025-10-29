31aa200fda2bbf94312bf987d6b3104ad85629b1
Andre goncalves <andrevilanovagon@hotmail.com>
2025-10-28 21:11:18 -0300

    feat: add validation annotations to CustomerInput and AddressData, and implement ApiExceptionHandler for error handling

--- src/main/java/com/algaworks/algashop/ordering/presentation/CustomerController.java
+++ src/main/java/com/algaworks/algashop/ordering/presentation/CustomerController.java
@@ -1,40 +1,57 @@
 package com.algaworks.algashop.ordering.presentation;
 
-import com.algaworks.algashop.ordering.application.commons.AddressData;
 import com.algaworks.algashop.ordering.application.customer.management.CustomerInput;
+import com.algaworks.algashop.ordering.application.customer.management.CustomerManagementApplicationService;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerFilter;
 import com.algaworks.algashop.ordering.application.customer.query.CustomerOutput;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerQueryService;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerSummaryOutput;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/customers")
+@RequiredArgsConstructor
 public class CustomerController {
+    
+    private final CustomerManagementApplicationService customerManagementApplicationService;
+    private final CustomerQueryService customerQueryService;
     
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CustomerOutput create(@RequestBody CustomerInput input) {
-        AddressData address = input.getAddress();
-        
-        return CustomerOutput.builder()
-                .id(UUID.randomUUID())
-                .phone(input.getPhone())
-                .email(input.getEmail())
-                .firstName(input.getFirstName())
-                .lastName(input.getLastName())
-                .birthDate(input.getBirthDate())
-                .document(input.getDocument())
-                .registeredAt(OffsetDateTime.now())
-                .promotionNotificationsAllowed(input.getPromotionNotificationsAllowed())
-                .archived(false)
-                .loyaltyPoints(0)
-                .address(address)
-                .build();
+    public CustomerOutput create(@RequestBody @Valid CustomerInput input, HttpServletResponse httpServletResponse) {
+        UUID customerId = customerManagementApplicationService.create(input);
+
+        UriComponentsBuilder builder = MvcUriComponentsBuilder.fromMethodCall(
+                MvcUriComponentsBuilder.on(CustomerController.class).findById(customerId)
+        );
+
+        httpServletResponse.addHeader("Location", builder.toUriString());
+                
+        return customerQueryService.findById(customerId);
     }
+    
+    @GetMapping
+    public PageModel<CustomerSummaryOutput> findAll(CustomerFilter filter) {
+        return PageModel.of(customerQueryService.filter(filter));
+    }
+    
+    @GetMapping("{customerId}")
+    public CustomerOutput findById(@PathVariable UUID customerId) {
+        return customerQueryService.findById(customerId);
+    }
+    
 }
